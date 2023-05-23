39dabf7b46a0caadae0af984ec4b39f6bc80e06c
Álex Cortiñas <mail@alexcortinas.es>
2023-05-23 10:28:57 +0200

    First test

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 import cli from "./src/cli.js";
 
-cli();
+cli(__dirname);
--- src/cli.js
+++ src/cli.js
@@ -1,16 +1,23 @@
 import meow from "meow";
 import fs from "fs";
+import Gisbuilder2 from "./main.js";
 import path from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const usage = fs.readFileSync(path.join(__dirname, "../usage.txt"), "utf8");
-
-export default function cli() {
+export default function cli(basepath) {
+  const usage = fs.readFileSync(path.join(basepath, "usage.txt"), "utf8");
   const cli = meow(usage, {
     importMeta: import.meta,
   });
 
-  console.log(cli);
+  const folder = cli.input.at(0);
+  if (!folder) {
+    cli.showHelp();
+  }
+
+  const bbox = cli.flags.bbox;
+
+  const gisbuilder2 = new Gisbuilder2(path.join(basepath, folder), bbox);
+  gisbuilder2.run();
+
+  console.log(`Running gisbuilder2 for folder ${folder} and bbox ${bbox}`);
 }
